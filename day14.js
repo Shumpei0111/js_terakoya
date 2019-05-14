68bce46b657066319c76da20550ba5a7a67884c6
Shumpei O <40765657+Shumpei0111@users.noreply.github.com>
2019-05-14 12:30:41 +0900

    Add day14.js

---
+++ day14.js
@@ -0,0 +1,19 @@
+// day13はcodesandboxに記述のためこのコードだと普通に読み込めません
+// 課題：https://codesandbox.io/s/oq8v130w4y
+
+// ライブラリ：typecheckerを読み込む
+
+// 課題
+// CodeSandbox でのライブラリの使い方に慣れる
+// 具体的には typechecker を Dependencies に追加し、テストで import する
+// 今回書いたテストを書いて、実行する
+
+import { getType } from "typechecker";
+
+describe("type", () => {
+  it("string", () => {
+    const string = 1;
+    const res = getType(string);
+    expect(res).toEqual();
+  });
+});
